819beb3308de31db2227fea4d762a629931412fd
Ar-Akhil <ar.akhil1511@gmail.com>
2024-07-11 12:48:31 +0530

    working image links

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,28 +17,36 @@ const Home = () => {
         </section>
         <section className="grid grid-cols-4 grid-rows-3 gap-3 py-8">
           <div className="col-span-2 blur-[2px] grayscale hover:filter-none">
-            <img
-              src={streetFight}
-              alt="streetfight project"
-              className="object-cover h-[100%]"
-            />
+            <a href="https://ar-akhil.github.io/RPS-Survival/" target="blank">
+              <img
+                src={streetFight}
+                alt="streetfight project"
+                className="object-cover h-[100%]"
+              />
+            </a>
           </div>
           <div className="col-span-3 row-span-2 blur-[2px] grayscale hover:filter-none ">
-            <img
-              src={weather}
-              alt="weather project"
-              className="object-cover h-[100%]"
-            />
+            <a href="https://ar-akhil.github.io/WeatherGaze" target="blank">
+              <img
+                src={weather}
+                alt="weather project"
+                className="object-cover h-[100%]"
+              />
+            </a>
           </div>
           <div className="row-span-2 blur-[2px] grayscale hover:filter-none">
-            <img src={fastfood} alt="fastfood project" className="" />
+            <a href="https://ar-akhil.github.io/Restaurant" target="blank">
+              <img src={fastfood} alt="fastfood project" className="" />
+            </a>
           </div>
           <div className="col-span-2 col-start-3 row-start-1 grayscale blur-[2px] hover:filter-none">
-            <img
-              src={etchasketch}
-              alt="etchasketch project"
-              className="object-cover h-[100%]"
-            />
+            <a href="https://ar-akhil.github.io/Etch-a-Sketch" target="blank">
+              <img
+                src={etchasketch}
+                alt="etchasketch project"
+                className="object-cover h-[100%]"
+              />
+            </a>
           </div>
         </section>
         <p className="text-base">
@@ -53,27 +61,42 @@ const Home = () => {
             <div>
               <ul className="flex gap-4 py-2">
                 <li className="p-2 border-2 rounded-xl">
-                  <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript">
+                  <a
+                    href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+                    target="blank"
+                  >
                     Javascript
                   </a>
                 </li>
                 <li className="p-2 border-2 rounded-xl">
-                  <a href="https://developer.mozilla.org/en-US/docs/Web/HTML">
+                  <a
+                    href="https://developer.mozilla.org/en-US/docs/Web/HTML"
+                    target="blank"
+                  >
                     Html
                   </a>
                 </li>
                 <li className="p-2 border-2 rounded-xl">
-                  <a href="https://developer.mozilla.org/en-US/docs/Web/CSS">
+                  <a
+                    href="https://developer.mozilla.org/en-US/docs/Web/CSS"
+                    target="blank"
+                  >
                     Css
                   </a>
                 </li>
                 <li className="p-2 border-2 rounded-xl">
-                  <a href="https://developer.mozilla.org/en-US/docs/Glossary/SQL">
+                  <a
+                    href="https://developer.mozilla.org/en-US/docs/Glossary/SQL"
+                    target="blank"
+                  >
                     SQL
                   </a>
                 </li>
                 <li className="p-2 border-2 rounded-xl">
-                  <a href="https://developer.mozilla.org/en-US/docs/Glossary/Java">
+                  <a
+                    href="https://developer.mozilla.org/en-US/docs/Glossary/Java"
+                    target="blank"
+                  >
                     Java
                   </a>
                 </li>
